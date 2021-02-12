648ad621091e0db6e8765c11e13fa36f03a33dbd
Ashif <ashiful134@gmail.com>
2021-02-12 17:45:33 +0600

    first commit

---
+++ app/Services/PostService.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\StorePostRequest;
+use App\Models\Post;
+use App\Repositories\PostRepository;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+
+class PostService
+{
+    /**
+     * @var $postRepository
+     */
+    protected $postRepository;
+
+    /**
+     * PostService constructor.
+     *
+     * @param PostRepository $postRepository
+     */
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    /**
+     * Delete post by id.
+     *
+     * @param $id
+     * @return String
+     */
+    public function deleteById($id)
+    {
+        DB::beginTransaction();
+
+        try {
+            $post = $this->postRepository->delete($id);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+
+            throw new InvalidArgumentException('Unable to delete post data');
+        }
+
+        DB::commit();
+
+        return $post;
+    }
+
+    /**
+     * Get all post.
+     *
+     * @return String
+     */
+    public function getAll()
+    {
+        return $this->postRepository->getAll();
+    }
+
+    /**
+     * Get post by id.
+     *
+     * @param $id
+     * @return String
+     */
+    public function getById($id)
+    {
+        return $this->postRepository->getById($id);
+    }
+
+    /**
+     * Update post data
+     * Store to DB if there are no errors.
+     *
+     * @param array $data
+     * @return String
+     */
+    public function updatePost(StorePostRequest $data, $id)
+    {  
+
+        DB::beginTransaction();
+
+        try {
+            $post = $this->postRepository->update($data, $id);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+
+            throw new InvalidArgumentException('Unable to update post data');
+        }
+
+        DB::commit();
+
+        return $post;
+    }
+
+    /**
+     * Validate post data.
+     * Store to DB if there are no errors.
+     *
+     * @param array $data
+     * @return String
+     */
+    public function savePostData($data)
+    {
+        $validator = Validator::make($data, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        $result = $this->postRepository->save($data);
+
+        return $result;
+    }
+}
